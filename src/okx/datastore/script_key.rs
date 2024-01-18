cd7ff2c2fa7c3483bd028c821bbf8e46ff8f8f01
shaneson <781244184@qq.com>
2024-01-18 14:49:48 +0800

    handle-script-address

--- src/okx/datastore/script_key.rs
+++ src/okx/datastore/script_key.rs
@@ -1,33 +1,6 @@
 use bitcoin::{Address, Network, Script, ScriptHash};
 use serde::{Deserialize, Serialize};
 use std::fmt::{Display, Formatter};
-use utoipa::ToSchema;
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, ToSchema)]
-#[serde(rename_all = "camelCase")]
-pub enum ScriptPubkey {
-  /// Address.
-  Address(String),
-  /// Non-standard script hash.
-  NonStandard(String),
-}
-
-impl Default for ScriptPubkey {
-  fn default() -> Self {
-    ScriptPubkey::NonStandard(String::new())
-  }
-}
-
-impl From<ScriptKey> for ScriptPubkey {
-  fn from(script_key: ScriptKey) -> Self {
-    match script_key {
-      ScriptKey::Address(address) => ScriptPubkey::Address(address.to_string()),
-      ScriptKey::ScriptHash(hash) => ScriptPubkey::NonStandard(hash.to_string()),
-    }
-  }
-}
-
-
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Serialize, Deserialize)]
 pub enum ScriptKey {
